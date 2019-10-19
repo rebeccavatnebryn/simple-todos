4a60869d4540513e82940c13b8084bd94f1eb777
Rebecca Vatnebryn <rebecca.vatnebryn@icloud.com>
2019-10-18 16:00:12 -1000

    Section 5

--- app/imports/ui/App.js
+++ app/imports/ui/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
-
 import { Tasks } from '../api/tasks.js';
-
 import Task from './Task.js';
 
 // App component - represents the whole app
